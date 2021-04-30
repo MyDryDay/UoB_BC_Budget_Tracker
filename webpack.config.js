fbc6cf7badb19b71f2c88ec6a3cd74d12c1ad4e0
George Cope <G.Cope13@gmail.com>
2021-04-30 09:51:43 +0100

    Added a webpack.config.json file

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+import WebpackPwaManifest from 'webpack-pwa-manifest';
+const path = require('path');
+
+const config = {
+    entry: {
+        index: './public/index.js',
+        database: './public/database.js',
+    },
+    output: {
+        path: __dirname + 'public/dist',
+        filename: '[name].bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [new WebpackPwaManifest({
+        fingerprints: false,
+        name: 'Online/Offline Budget Tracker',
+        short_name: 'Budget Tracker PWA',
+        orientation: 'portrait',
+        display: 'standalone',
+        start_url: '/',
+        description: 'A personal budget tracker with online & offline functionality',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: path.resolve('public/icons/icon-512x512.png'),
+                sizes: [72, 96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons'),
+            },
+        ],
+
+    }),
+    ],
+};
+
+module.exports = config;
